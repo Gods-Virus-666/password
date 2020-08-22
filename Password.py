8c3936e818e1d584570541796bb2b919ba9c76d5
Gods-Virus-666 <62434533+Gods-Virus-666@users.noreply.github.com>
2020-08-22 01:09:56 -0500

    Update Password.py

--- Password.py
+++ Password.py
@@ -1,6 +1,5 @@
 password = int(input("Enter Password: "))
 b = int((7827156067/password))
 a = int((password*b))
-if int(7827156067) == int(a)
-  Print("Successful Entry")
-Print("Error 404")
+a == int(7827156067) 
+print(a)
