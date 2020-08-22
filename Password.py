84e39b0ce0d78e7d2ef4be92b28200b4fd356241
Gods-Virus-666 <62434533+Gods-Virus-666@users.noreply.github.com>
2020-08-22 01:19:29 -0500

    Update Password.py

--- Password.py
+++ Password.py
@@ -1,6 +1,7 @@
 password = int(input("Enter Password: "))
 b = int(7827156067) / int(password)
 a = int(password) * int(b)
-if a == int(7827156067)
-  Print("successful"
+c = int(7827156067)
+if a == c
+  Print("successful")
 Print("error")
