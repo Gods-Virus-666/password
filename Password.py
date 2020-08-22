a34e31af35d4345b787806fd0645207ce2198e56
Gods-Virus-666 <62434533+Gods-Virus-666@users.noreply.github.com>
2020-08-22 02:38:19 -0500

    Update Password.py

--- Password.py
+++ Password.py
@@ -1,8 +1,9 @@
 password = int(input("Enter Password: "))
-b = int(7827156067) / int(password)
-a = int(password) * int(b)
-c = int(7827156067)
-if a == c:
+b = 7827156067 / password
+a = password * int(b)
+c = 7827156067
+if a == c and password not in [1, c]:
   print("Success! Encryption Key Match")
 else:
   print("Failure.. Encrypting Data")
+ 
